ad877ff9115a62feb8be387f0a0a1796bd2572e6
Youness Bourajli <happyworkyouness@gmail.com>
2026-09-25 14:26:38 +0000

    Training

---
+++ python_language/just_python/dataclassess.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass(slots=True)
+class car:
+    """Class for keeping track of an item in inventory."""
+    name: str
+    modele: int
+    hors_power: int
+
+bmw = car("bmw_m5", "2024", 95)
+
+print(bmw.name)
+print(bmw.hors_power)
+print(bmw.modele)
